71b47899200761cb21539c1658dcdec93688cd65
Youness ZIOUANE <y.ziouan@prismamedia.fr>
2021-09-12 05:24:28 +0200

    first commit

---
+++ src/Controller/MainController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Controller;
+
+use App\Handlers\TheMovieDBHttpClientHandler;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Tmdb\ApiToken;
+use Tmdb\Client;
+
+class MainController extends AbstractController
+{
+
+    /** @var TheMovieDBHttpClientHandler */
+    private $client;
+
+    /**
+     * MainController constructor.
+     * @param TheMovieDBHttpClientHandler $client
+     */
+    public function __construct(TheMovieDBHttpClientHandler $client)
+    {
+        $this->client = $client;
+    }
+
+
+    /**
+     * @Route("/", name="home")
+     */
+    public function index() : Response
+    {
+        $genders = $this->client->getGenders();
+        $movies = $this->client->getUpcoming();
+        $latestMovieTrailer = $this->client->getTrailers($movies['results'][0]['id']);
+        return $this->render('home/index.html.twig', [
+            'latest' => $latestMovieTrailer,
+            'genders'   => $genders,
+            'movies' => $movies,
+        ]);
+    }
+
+    /**
+     * @Route("/search",name="search")
+     */
+    public function search(Request $request): Response
+    {
+        $search = trim($request->get('search'));
+        if ($search == "") {
+            return $this->render('movies_list/no_match.html.twig');
+        } else {
+
+            $searchByTitle = $this->client->searchMovies($search);
+
+            return $this->render('movie/search.html.twig', [
+                'bytitles' => $searchByTitle["results"],
+            ]);
+        }
+
+    }
+
+    /**
+     * @Route("movie/{id}", name="movie_details")
+     */
+    public function movieDetails($id) : Response
+    {
+
+        $movie = $this->client->getMovie($id);
+        $credits = $this->client->getCredits($id);
+        $trailers = $this->client->getTrailers($id);
+
+        return $this->render('movie/details.html.twig',[
+            'movie' => $movie,
+            'credits' => $credits,
+            'trailers'=> $trailers
+        ]);
+    }
+}
---
+++ src/Handlers/TheMovieDBHttpClientHandler.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Handlers;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class TheMovieDBHttpClientHandler
+{
+
+    private HttpClientInterface $client;
+
+    private string $url;
+    private string $token;
+
+    private array $headers = [];
+
+    public function __construct(HttpClientInterface $client, $url, $token)
+    {
+        $this->client = $client;
+        $this->url = $url;
+        $this->token = $token;
+
+        $this->headers = [
+            'Content-Type'  => 'application/json',
+            'Authorization' => 'Bearer ' . $this->token,
+        ];
+    }
+
+    public function getLatest(array $parameters = [], array $headers = [])
+    {
+        return $this->call('movie/latest', $parameters, $headers);
+    }
+
+    public function getUpcoming(array $parameters = [], array $headers = [])
+    {
+        return $this->call('movie/upcoming', $parameters, $headers);
+    }
+
+    public function getGenders(array $parameters = [], array $headers = [])
+    {
+        return $this->call('genre/movie/list', $parameters, $headers);
+    }
+
+    public function getGenderMovies($genre_id, array $parameters = [], array $headers = [])
+    {
+        return $this->call('genre/' . $genre_id . '/movies', $parameters, $headers);
+    }
+
+    public function getNowPlaying(array $parameters = [], array $headers = [])
+    {
+        return $this->call('movie/now_playing', $parameters, $headers);
+    }
+
+    public function searchMovies($query, array $parameters = [], array $headers = [])
+    {
+        return $this->call('search/movie', array_merge($parameters, [
+            'query' => $query
+        ], $headers));
+    }
+
+    public function getMovie($movie_id, array $parameters = [], array $headers = [])
+    {
+        return $this->call('movie/' . $movie_id, $parameters, $headers);
+    }
+
+    public function getCredits($movie_id, array $parameters = [], array $headers = [])
+    {
+        return $this->call('movie/' . $movie_id . '/credits', $parameters, $headers);
+    }
+
+    public function getTrailers($movie_id, array $parameters = [], array $headers = [])
+    {
+        return $this->call('movie/' . $movie_id . '/trailers', $parameters, $headers);
+    }
+
+    private function call(string $endpoint, array $params = [], array $headers = []): array
+    {
+        try {
+            $querystring = '?' . http_build_query($params);
+
+
+            $response = $this->client->request('GET', $this->url . $endpoint . $querystring, ['headers' => array_merge($this->headers, $headers)]);
+
+            return (Response::HTTP_OK) ? $response->toArray() : [];
+        } catch (\Exception $e) {
+            dump($e->getMessage(), $e->getFile(), $e->getLine());
+            die;
+        }
+    }
+}
